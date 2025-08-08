67bb8c934a29159edc4d2bccdf36c014beb072d8
Malinsha Vithanage <86399536+MalinshaVithanage@users.noreply.github.com>
2025-08-08 22:18:52 +0530

    updated subscription form

--- user/src/app/pages/about-us/about-us.component.ts
+++ user/src/app/pages/about-us/about-us.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SubscriptionFormComponent } from "../../subscription-form/subscription-form.component";
 
 @Component({
   selector: 'app-about-us',
   standalone: true,
-  imports: [],
+  imports: [SubscriptionFormComponent],
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.css'
 })
