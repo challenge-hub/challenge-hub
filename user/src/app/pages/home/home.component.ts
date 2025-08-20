5fe925bbcbcae14bf6974633d400f705904d67e9
Malinsha Vithanage <86399536+MalinshaVithanage@users.noreply.github.com>
2025-08-20 05:31:09 +0530

    home page posts loading done

--- user/src/app/pages/home/home.component.ts
+++ user/src/app/pages/home/home.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
-import { PostCardComponent } from "../../layouts/post-card/post-card.component";
+import { PostCardComponent } from '../../layouts/post-card/post-card.component';
+import { PostsService } from '../../services/posts.service';
+import { NgForOf } from '@angular/common';
 
 @Component({
-    selector: 'app-home',
-    standalone: true,
-    templateUrl: './home.component.html',
-    styleUrl: './home.component.css',
-    imports: [PostCardComponent]
+  selector: 'app-home',
+  standalone: true,
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+  imports: [PostCardComponent, NgForOf],
 })
 export class HomeComponent {
+  featuredPostsArray: any[] = [];
+    latestPostsArray: any[] = [];
 
+  constructor(private postServices: PostsService) {}
+    ngOnInit(): void {
+    this.postServices.loadFeatured().subscribe((val) => {
+      this.featuredPostsArray = val;
+    });
+    this.postServices.loadLatest().subscribe((val) => {
+      this.latestPostsArray = val;
+    });
+  }
 }
---
+++ user/src/app/services/posts.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { collection, getDocs, query, where, Firestore, limit } from '@angular/fire/firestore';
+import { ref } from '@angular/fire/storage';
+import { orderBy } from 'firebase/firestore';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostsService {
+
+  constructor(private afs: Firestore) { }
+
+   loadFeatured(): Observable<any[]> {
+    return new Observable<any[]>((observer) => {
+      const postsRef = collection(this.afs, 'posts');
+      const q = query(postsRef, where('isFeatured', '==', true), limit(4));
+
+      getDocs(q)
+        .then((querySnapshot) => {
+          const data: any[] = [];
+          querySnapshot.forEach((doc) => {
+            data.push({ id: doc.id, ...doc.data() });
+          });
+          observer.next(data);
+          observer.complete();
+        })
+        .catch((error) => {
+          observer.error(error);
+        });
+    });
+  }
+
+  loadLatest(): Observable<any[]> {
+ return new Observable<any[]>((observer) => {
+      const postsRef = collection(this.afs, 'posts');
+      const q = query(postsRef, orderBy('createdAt'));
+
+      getDocs(q)
+        .then((querySnapshot) => {
+          const data: any[] = [];
+          querySnapshot.forEach((doc) => {
+            data.push({ id: doc.id, ...doc.data() });
+          });
+          observer.next(data);
+          observer.complete();
+        })
+        .catch((error) => {
+          observer.error(error);
+        });
+    });
+  }
+}
